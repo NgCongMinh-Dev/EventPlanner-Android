5a852ed42420dc7326dfcb5d28c538fab27532ec
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2020-08-28 11:54:41 +0200

    init project and login view

---
+++ app/src/main/java/com/htw/project/eventplanner/activity/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.htw.project.eventplanner.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.htw.project.eventplanner.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle(R.string.title_activity_login);
+        setContentView(R.layout.activity_login);
+
+        bindView();
+
+    }
+
+    private void bindView() {
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                System.err.println(username + " " + password);
+            }
+        });
+    }
+
+}
