bce0f97ad843c5eea5608d3b20d5c15ae5628799
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2020-09-28 02:15:22 +0200

    fix scroll behavior

--- app/src/main/java/com/htw/project/eventplanner/View/TaskSectionView.java
+++ app/src/main/java/com/htw/project/eventplanner/View/TaskSectionView.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -117,7 +118,7 @@ public class TaskSectionView extends LinearLayout {
         taskContainer.setVisibility(GONE);
         taskContainer.setLayoutManager(new LinearLayoutManager(getContext()));
         taskContainer.setItemAnimator(new DefaultItemAnimator());
-        taskContainer.setNestedScrollingEnabled(false);
+        taskContainer.setOverScrollMode(View.OVER_SCROLL_NEVER);
         addView(taskContainer);
     }
 
